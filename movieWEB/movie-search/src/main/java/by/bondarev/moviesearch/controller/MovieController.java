f3687d8e3732218403b19493b9ab980e5990e96e
wupdp <kirillbond2017@gmail.com>
2024-02-25 00:58:12 +0300

    rename src dir

---
+++ movieWEB/movie-search/src/main/java/by/bondarev/moviesearch/controller/MovieController.java
@@ -0,0 +1,29 @@
+package by.bondarev.moviesearch.controller;
+
+import by.bondarev.moviesearch.service.MovieService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import java.io.IOException;
+
+@RestController
+public class MovieController {
+
+    private final MovieService movieService;
+
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @GetMapping("/movie/info")
+    public ResponseEntity<String> getMovieInfo(@RequestParam("title") String title) {
+        try {
+            String movieInfo = movieService.getMovieInfo(title);
+            return ResponseEntity.ok(movieInfo);
+        } catch (IOException e) {
+            return new ResponseEntity<>("Error getting movie info", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
---
+++ movieWEB/movie-search/src/main/java/by/bondarev/moviesearch/service/MovieService.java
@@ -0,0 +1,19 @@
+package by.bondarev.moviesearch.service;
+
+import by.bondarev.moviesearch.kinopoiskAPI.KinopoiskAPI;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+public class MovieService {
+    private final KinopoiskAPI kinopoiskAPI;
+
+    public MovieService(KinopoiskAPI kinopoiskAPI) {
+        this.kinopoiskAPI = kinopoiskAPI;
+    }
+
+    public String getMovieInfo(String title) throws IOException {
+        return kinopoiskAPI.searchMovie(title).string();
+    }
+}
